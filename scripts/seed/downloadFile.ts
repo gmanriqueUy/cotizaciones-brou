bedc8b8a1fe8e159bcd7abb9e432bd15d70c9efb
Gonzalo Manrique <gonzalomanriquedeleon@gmail.com>
2023-04-03 20:27:30 -0300

    Update excel URL

--- scripts/seed/downloadFile.ts
+++ scripts/seed/downloadFile.ts
@@ -1,5 +1,5 @@
 const URL =
-  "https://www.ine.gub.uy/c/document_library/get_file?uuid=1dcbe20a-153b-4caf-84a7-7a030d109471";
+  "https://www5.ine.gub.uy/documents/Estad%C3%ADsticasecon%C3%B3micas/SERIES%20Y%20OTROS/Cotizaci%C3%B3n%20de%20monedas/Cotizaci%C3%B3n%20monedas.xlsx";
 
 export async function downloadFile() {
   return (await fetch(URL)).arrayBuffer();
