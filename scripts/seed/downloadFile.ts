02a2a181ba87d1d299194ad02df6ffbfbceee50a
Gonzalo <gonzalomanriquedeleon@gmail.com>
2024-05-19 14:25:11 -0300

    Update source link

--- scripts/seed/downloadFile.ts
+++ scripts/seed/downloadFile.ts
@@ -2,7 +2,7 @@ import { fetch } from "@remix-run/node";
 import { readFile } from "fs/promises";
 
 const URL =
-  "https://www5.ine.gub.uy/documents/Estad%C3%ADsticasecon%C3%B3micas/SERIES%20Y%20OTROS/Cotizaci%C3%B3n%20de%20monedas/Cotizaci%C3%B3n%20monedas.xlsx";
+  "https://www5.ine.gub.uy/documents/Estad%C3%ADsticasecon%C3%B3micas/SERIES%20Y%20OTROS/Cotizaci%C3%B3n%20monedas/Cotizaci%C3%B3n%20monedas.xlsx";
 
 export async function downloadFile() {
   const response = await fetch(URL);
